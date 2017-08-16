7bafcf3730e6181123e0fc59298e0572deb2d29b
Carlos Schults <carlos.schults@gmail.com>
2017-08-16 16:59:27 -0300

    Término dos testes.

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -1,9 +1,25 @@
-﻿namespace StringCalculatorKata
+﻿using System;
+
+namespace StringCalculatorKata
 {
     public class StringCalculator
     {
-        public StringCalculator()
+        public int Add(string valor)
         {
+            int resultado = 0;
+            var partes = valor.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (string parte in partes)
+            {
+                int numero = int.Parse(parte);
+
+                if (numero < 0)
+                    throw new ArgumentException("Números negativos não são permitidos.");
+
+                resultado += numero;
+            }
+
+            return resultado;
         }
     }
 }
--- StringCalculatorTest/StringCalculatorTest.cs
+++ StringCalculatorTest/StringCalculatorTest.cs
@@ -13,11 +13,91 @@ namespace StringCalculatorTest
         [Test]
         public void StringCalculator_String_Vazia_Deve_Retornar_Zero()
         {
-            var valor = string.Empty;
-            var sut = new StringCalculator();
-            var esperado = 0;
-            var obtido = sut.Add(valor);
+            // arrange
+            string valor = string.Empty;
+            StringCalculator sut = new StringCalculator();
+            int esperado = 0;
+
+            // act
+            int obtido = sut.Add(valor);
+
+            // assert
             Assert.AreEqual(esperado, obtido);
+        }
+
+        [TestCase("5", 5)]
+        [TestCase("1", 1)]
+        [TestCase("10", 10)]
+        [TestCase("7", 7)]        
+        public void StringCalculator_String_Contendo_Um_Numero_Deve_Retornar_O_Proprio_Numero(string valor, int esperado)
+        {
+            // arrange
+            StringCalculator sut = new StringCalculator();
+
+            // act
+            int obtido = sut.Add(valor);
+
+            // assert
+            Assert.AreEqual(esperado, obtido);
+        }
+
+        [TestCase("5, 0", 5)]
+        [TestCase("1, 1", 2)]
+        [TestCase("10, 3", 13)]
+        [TestCase("7, 4", 11)]
+        public void StringCalculator_String_Contendo_Dois_Numeros_Deve_Retornar_Sua_Soma(string valor, int esperado)
+        {
+            // arrange
+            StringCalculator sut = new StringCalculator();
+
+            // act
+            int obtido = sut.Add(valor);
+
+            // assert
+            Assert.AreEqual(esperado, obtido);
+        }
+
+        [TestCase("5, 0, 3", 8)]
+        [TestCase("1, 1, 1", 3)]
+        [TestCase("10, 3, 5", 18)]
+        [TestCase("7, 4, 1", 12)] 
+        public void StringCalculator_String_Contendo_Tres_Numeros_Deve_Retornar_Sua_Soma(string valor, int esperado)
+        {
+            // arrange
+            StringCalculator sut = new StringCalculator();
+
+            // act
+            int obtido = sut.Add(valor);
+
+            // assert
+            Assert.AreEqual(esperado, obtido);
+        }
+
+        [TestCase("5", 5)]
+        [TestCase("1, 1", 2)]
+        [TestCase("10, 3", 13)]
+        [TestCase("7, 4, 1, 3", 15)]
+        public void StringCalculator_String_Contendo_Qualquer_Quantidade_De_Numeros_Deve_Retornar_Sua_Soma(string valor, int esperado)
+        {
+            // arrange
+            StringCalculator sut = new StringCalculator();
+
+            // act
+            int obtido = sut.Add(valor);
+
+            // assert
+            Assert.AreEqual(esperado, obtido);
+        }
+
+        [TestCase("-5")]
+        [TestCase("-1")]
+        [TestCase("10, -3")]
+        [TestCase("7, -4, 1, -3")]
+        [TestCase("-1, 1, 1, 2, 3")]
+        public void StringCalculator_String_Contendo_Numero_Negativo_Deve_Disparar_Excecao(string valor)
+        {
+            StringCalculator sut = new StringCalculator();
+            Assert.That(() => sut.Add(valor), Throws.TypeOf<ArgumentException>());
         }
     }
 }
