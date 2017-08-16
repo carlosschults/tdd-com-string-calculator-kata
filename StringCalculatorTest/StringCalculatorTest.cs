e11694a136849acc5d07887631969a1031f7f592
Carlos Schults <carlos.schults@gmail.com>
2017-08-16 16:18:09 -0300

    Adição da classe de teste e criação do esqueleto do primeiro método de teste.

---
+++ StringCalculatorTest/StringCalculatorTest.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculatorTest
+{
+    class StringCalculatorTest
+    {
+        [Test]
+        public void StringCalculator_String_Vazia_Deve_Retornar_Zero()
+        {
+            
+        }
+    }
+}
